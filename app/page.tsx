5d7ebba22441db330d37d6df01ef8d7bc743efa4
Uche44 <perpetualuchechukwu808@gmail.com>
2026-01-12 20:52:04 +0100

    changed loading color

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ const Home = () => {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-card">
-      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-purple-600"></div>
+      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-green-600"></div>
     </div>
   );
 };
